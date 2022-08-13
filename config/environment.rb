85fdfafe0db1f5eeb71535891ab20a7f20a19f41
danny galimi <dannygalimi13@aim.com>
2022-08-13 08:53:58 -0400

    set environment to ENV[SINATRA_ENV], set database_config variable as hash with development key equal to hash to :adapter of sqlite3 and database of db/#{ENV['SINATRA_ENV']}.sqlite

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ ENV['RAILS_ENV'] = ENV['SINATRA_ENV']
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
+
+set :environment, ENV['SINATRA_ENV'].to_sym
+
+database_config = {
+    :development => {
+        :adapter => "sqlite3",
+        :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+    }
+}
