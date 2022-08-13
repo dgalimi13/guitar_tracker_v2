c06a83201eb024a04385f7f25b5e78a6d176c212
danny galimi <dannygalimi13@aim.com>
2022-08-13 13:08:33 -0400

    add delete route to GamesController, controller action destroys guitar with id value from parmams[:id] and redirects to its brand show page

--- app/controllers/guitars_controller.rb
+++ app/controllers/guitars_controller.rb
@@ -1,6 +1,10 @@
 class GamesController < ApplicationController
 
-    
+    delete "/guitars/:id" do
+        @guitar = Guitar.find_by(id: params[:id])
+        @guitar.destroy
+        redirect to "/brands/#{@guitar.brand.id }"
+    end
     
     
     end 
