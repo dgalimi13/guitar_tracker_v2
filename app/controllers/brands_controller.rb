eabeca4b52a7c1c6e141593d7944366c577ad94a
danny galimi <dannygalimi13@aim.com>
2022-08-13 13:32:14 -0400

    add post '/brands' route. action checks if params[:name] is empty. if not, create a new Brand instince of Brand with appropriate attributes from params hash. if save is successful redirect to bands index, if not render brands new page

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -9,4 +9,16 @@ class BrandsController < ApplicationController
         erb :"brands/new.html"
     end
 
+    post '/brands' do 
+        if params[:name] != ""
+        @brand = Brand.new
+        @brand.name = params[:name]
+        @brand.user = current_user
+        @brand.save 
+            redirect '/brands'
+        else 
+            erb :'brands/new.html'
+        end
+    end
+
 end
