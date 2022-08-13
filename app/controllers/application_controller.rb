82175dc776883c51b00aeb895994b57ef1d82d45
danny galimi <dannygalimi13@aim.com>
2022-08-13 10:42:22 -0400

    add configure call with block that sets sessions enabled with a session secret and sets :views value to app/views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < Sinatra::Base
     register Sinatra::ActiveRecordExtension
     use Rack::Flash
+
+    configure do
+        enable :sessions
+        set :session_secret, "video_game_tracker-1234"
+        
+        set :public_folder, 'public'
+        set :views, 'app/views'
+      end
   
 end
   
