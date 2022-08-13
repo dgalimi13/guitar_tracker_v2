b3051bd801374c0942ec32b0e647dae26ce0df3d
danny galimi <dannygalimi13@aim.com>
2022-08-13 12:47:31 -0400

    add post 'users' route, route will redirect to home page if successful, render new/user for if not

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,16 @@ class UsersController < ApplicationController
         erb :'users/new.html'
     end 
 
+    post '/users' do
+        @user = User.new
+        @user.email = params[:email]
+        @user.password = params[:password]
+
+        if @user.save 
+            redirect "/"
+        else
+            erb :'users/new.html'
+        end
+    end
+
 end 
